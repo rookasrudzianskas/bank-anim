c10e75a3278373d04405d614b74b78799ccb1eef
rokas <rokas@rokas.com>
2024-04-19 18:34:13 +0300

    Add babel plugin for decorators and expo-build-properties

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -17,28 +17,37 @@ export default function TabOneScreen() {
     <View className="m-5">
       <AccountsList />
 
-      <View className={'flex flex-row items-center mb-3 mt-1 py-1 border border-white rounded-md '}>
+      <View className={'flex flex-row items-center mb-3 mt-1 py-1 justify-between border border-white rounded-md '}>
         <View className={'text-white flex-1 font-semibold'}>
           <TextInput
             className={'h-10 my-1 pl-3 text-white flex-1'}
             placeholder={'Name'}
-          />
-        </View>
-        <View className={'text-white items-center h-16 flex-1 font-semibold'}>
-          <TextInput
-            className={'h-10 my-1 text-white flex-1'}
-            placeholder={'Enter'}
-          />
-          <Text className={'text-white'}>Cap</Text>
-        </View>
-        <View className={'flex items-center h-16 flex-1 flex-col'}>
-          <TextInput
-            className={'h-10 flex-1 my-1 text-white'}
-            placeholder={'Enter'}
             value={name}
             onChangeText={setName}
           />
-          <Text className={'text-white'}>Cap</Text>
+        </View>
+        <View className={'flex-1 flex flex-row'}>
+          <View className={'text-white items-center h-16 flex-1 font-semibold'}>
+            <TextInput
+              className={'h-10 my-1 text-white flex-1'}
+              placeholder={'Enter'}
+            />
+            <Text className={'text-white'}>Cap</Text>
+          </View>
+          <View className={'flex items-center h-16 flex-1 flex-col'}>
+            <TextInput
+              className={'h-10 flex-1 my-1 text-white'}
+              placeholder={'Enter'}
+            />
+            <Text className={'text-white'}>Cap</Text>
+          </View>
+          <View className={'flex items-center h-16 flex-1 flex-col'}>
+            <TextInput
+              className={'h-10 flex-1 my-1 text-white'}
+              placeholder={'Enter'}
+            />
+            <Text className={'text-white'}>Cap</Text>
+          </View>
         </View>
       </View>
 
