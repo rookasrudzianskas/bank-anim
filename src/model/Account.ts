7f922baa5b5089135cbd6827e98299d8c8559f74
rokas <rokas@rokas.com>
2024-04-20 08:55:50 +0300

    Add schema migrations and create Account model

---
+++ src/model/Account.ts
@@ -0,0 +1,11 @@
+// @ts-nocheck
+import { Model } from '@nozbe/watermelondb';
+import { field, text } from '@nozbe/watermelondb/decorators';
+
+export default class Account extends Model {
+  static table = 'accounts';
+
+  @text('name') name: string;
+  @field('cap') cap: number;
+  @field('tap') tap: number;
+}
