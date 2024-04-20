937d752bf8d440a8ee7790b97823e290b3818b81
rokas <rokas@rokas.com>
2024-04-20 09:44:15 +0300

    Update display of account information in components

--- src/components/account-list-item.tsx
+++ src/components/account-list-item.tsx
@@ -1,21 +1,26 @@
 //@ts-nocheck
 import React from 'react';
 import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
+import Account from '../model/Account';
 
-const AccountListItem = () => {
+type AccountListItem = {
+  account: Account;
+};
+
+const AccountListItem = ({account}: AccountListItem) => {
   return (
     <TouchableOpacity activeOpacity={0.7} className={'flex flex-row my-1 items-center border rounded-md border-black' +
       ' border-1' +
       ' justify-between px-7 py-3' +
       ' text-black'}>
-      <Text className={'text-black text-lg font-semibold'}>Profit</Text>
+      <Text className={'text-black text-lg font-semibold'}>{account?.name}</Text>
       <View className={'flex flex-row items-center gap-6'}>
         <View className={'flex flex-col items-center justify-center'}>
-          <Text className={'text-black text-lg font-semibold'}>10%</Text>
+          <Text className={'text-black text-lg font-semibold'}>{account?.cap}%</Text>
           <Text className={'text-black'}>Cap</Text>
         </View>
         <View className={'flex flex-col items-center justify-center'}>
-          <Text className={'text-black text-lg font-semibold'}>10%</Text>
+          <Text className={'text-black text-lg font-semibold'}>{account?.tap}%</Text>
           <Text className={'text-black'}>Cap</Text>
         </View>
         <View className={'flex flex-col items-center justify-center'}>
--- src/components/accounts-list.tsx
+++ src/components/accounts-list.tsx
@@ -1,18 +1,31 @@
 //@ts-nocheck
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, View, StyleSheet, FlatList} from 'react-native';
 import AccountListItem from "@/src/components/account-list-item";
+import {awaitExpression} from "@babel/types";
+import {accountsCollection} from "@/src/db";
+import Account from "@/src/model/Account";
 
 export const DATA = [1, 2, 3, 4, 5, 6]
 
 const AccountsLists = () => {
+  const [accounts, setAccounts] = useState<Account[]>([]);
+  useEffect(() => {
+    const fetchAccounts = async () => {
+      const accounts = await accountsCollection.query().fetch();
+      console.log(accounts)
+      setAccounts(accounts)
+    }
+    fetchAccounts();
+  }, [])
+
   return (
     <>
       <FlatList
-        data={DATA}
+        data={accounts.slice(0, 5)}
         renderItem={(item) => {
           return (
-            <AccountListItem />
+            <AccountListItem account={item} />
           )
         }}
         contentContainerStyles={{
